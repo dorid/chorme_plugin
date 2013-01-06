8afed533e8d33b51d2e06ed9f98cb03c4e37f12f
dorid <562623566@qq.com>
2013-01-06 09:46:57 +0800

    t

---
+++ web/js/hello.js
@@ -0,0 +1,7 @@
+
+$("#nv").append("<button class='long_button_u_down' type='button' id='btnAutoSubmit'>自动提交</button> <button class='long_button_u_down' type='button' id='btnCancelAuto'>取消自动</button>");
+alert($("#nv").text());
+
+$("#btnAutoSubmit").click(function(){
+    alert("fuck baidu!!");
+});
