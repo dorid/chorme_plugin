a6b56be24e89c75e03a3e45666abb664b4ef8871
dorid <562623566@qq.com>
2013-01-10 17:46:26 +0800

    t

--- web/js/hello.js
+++ web/js/hello.js
@@ -1,7 +1,44 @@
 
 $("#nv").append("<button class='long_button_u_down' type='button' id='btnAutoSubmit'>自动提交</button> <button class='long_button_u_down' type='button' id='btnCancelAuto'>取消自动</button>");
-alert($("#nv").text());
+
+var utility = {
+    post: function (url, data, dataType, succCallback, errorCallback) {
+        $.ajax({
+            url: url,
+            data: data,
+            timeout: 30000,
+            type: "POST",
+            success: succCallback,
+            error: errorCallback,
+            dataType: dataType
+        });
+    },
+    get: function (url) {
+        $.ajax({
+            url: url,
+            async: false,
+            timeout: 30000,
+            type: "GET",
+            success: function(json){
+                alert(json.test);
+            },
+            error: function(data){
+                console.log(data);
+            },
+            dataType: "jsonp"
+        });
+    }
+}
 
 $("#btnAutoSubmit").click(function(){
-    alert("fuck baidu!!");
+    //utility.get("http://localhost:8080/ot125?cookie1=ttttt");
+
+    $.getJSON('http://localhost:8080/ot125?cookie1=ttttt&callback=test', function(data){
+        //处理data数据
+        console.log(data);
+    });
 });
+
+function test(data){
+    console.log(data);
+}
